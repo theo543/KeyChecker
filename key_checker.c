036e2ef19955a2dd4800bee073bd68419f665559
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-12-02 16:43:36 +0200

    Add static text

--- key_checker.c
+++ key_checker.c
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include <stdio.h>
 
 const wchar_t STATIC_CLASS[] = TEXT("STATIC");
 const wchar_t WINDOW_CLASS[] = TEXT("Key Checker");
@@ -9,8 +10,17 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT wm, WPARAM wp, LPARAM lp);
 const int WINDOW_WIDTH_INIT = 400;
 const int WINDOW_HEIGHT_INIT = 200;
 
+wchar_t TXT_BUFFER[100] = {};
+const int TXT_BUFFER_SIZE = sizeof(TXT_BUFFER) / sizeof(TXT_BUFFER[0]);
+
+HWND hwndStaticText = NULL;
+
+HINSTANCE hInst = NULL;
+
+const int TIMER_MS = 5; // 5 ms = 200 Hz
+
 int main() {
-    HINSTANCE hInst = GetModuleHandle(NULL);
+    hInst = GetModuleHandle(NULL);
 
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInst;
@@ -19,31 +29,50 @@ int main() {
     RegisterClass(&wc);
 
     HWND hwnd = CreateWindowEx(
-        0,
-        WINDOW_CLASS,
-        TEXT("Key Checker"),
-        WS_OVERLAPPEDWINDOW,
+            0,
+            WINDOW_CLASS,
+            TEXT("Key Checker"),
+            WS_OVERLAPPEDWINDOW,
 
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        HORIZONTAL,
-        VERTICAL,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            WINDOW_WIDTH_INIT,
+            WINDOW_HEIGHT_INIT,
 
-        NULL,
-        NULL,
-        hInst,
-        NULL
+            NULL,
+            NULL,
+            hInst,
+            NULL
+    );
+
+    hwndStaticText = CreateWindowEx(
+            0,
+            STATIC_CLASS,
+            NULL,
+        WS_CHILD | WS_VISIBLE,
+
+            0,
+            0,
+            WINDOW_WIDTH_INIT,
+            WINDOW_HEIGHT_INIT,
+
+            hwnd,
+            NULL,
+            hInst,
+
+            NULL
     );
 
     ShowWindow(hwnd, SW_SHOWNORMAL);
 
+    SetTimer(hwnd, 0, TIMER_MS, NULL);
     MSG msg = {};
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
 
-    return msg.wParam;
+    return (int)msg.wParam;
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT wm, WPARAM wp, LPARAM lp) {
@@ -51,6 +80,10 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT wm, WPARAM wp, LPARAM lp) {
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
+
+        case WM_TIMER:
+            swprintf(TXT_BUFFER, TXT_BUFFER_SIZE, TEXT("Hello"));
+            SetWindowText(hwndStaticText, TXT_BUFFER);
 
         default:
             return DefWindowProc(hwnd, wm, wp, lp);
